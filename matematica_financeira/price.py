3ac8bcc4967711fa17d1c5198afd4a63b7c677b6
Plinio Vianna <gangss2@hotmail.com>
2023-02-25 15:15:36 -0300

    +Price.py

--- matematica_financeira/price.py
+++ matematica_financeira/price.py
@@ -11,20 +11,35 @@ def generate_question():
     # Define o número de períodos
     n_periods = random.randint(1, 30)
     
+    # Define um mês meta para cálculo dos juros
+    monthly_meta = random.randint(1, n_periods)
+    
+    # dados do desafio extraído.
+    # loan_amount = 15000
+    # n_periods = 18
+    # interest_rate = 0.02
+    # monthly_meta = 10
+
     # Calcula a prestação mensal pelo sistema PRICE
     monthly_payment = (loan_amount * interest_rate) / (1 - (1 + interest_rate)**(-n_periods))
     
+    # Prevendo resultado
+    first_juros = loan_amount * (1 + interest_rate) - loan_amount # Juros do primeiro mês
+    first_amortization = monthly_payment - first_juros # Amortização do primeiro mês descontado o primeiro mês
+    meta_amortization = first_amortization * (1 + interest_rate) ** (monthly_meta-1)
+    meta_juros = monthly_payment - meta_amortization
+
     # Gera a questão com os valores obtidos
-    question = f"Qual o valor da prestação mensal de um empréstimo de R${loan_amount:.2f}, com taxa de juros de {interest_rate:.2%} ao mês, no sistema de amortização PRICE, em {n_periods} meses?"
+    question = f"Um empréstimo contraído no mês X0, no valor de R${loan_amount:.2f} deve ser pago em {n_periods} prestações mensais iguais, a uma taxa de juros compostos de {interest_rate*100:.2f}% ao mês, vencendo a primeira prestação no fim do mês X0, a segunda no fim de X1 e assim sucessivamente. Calcule quanto está sendo pago de juros na {monthly_meta} prestação."
     
     # Define a resposta correta
-    correct_answer = f"O valor da prestação mensal é de R${monthly_payment:.2f}."
+    correct_answer = f"O valor da prestação mensal é de R${monthly_payment:.2f}. E o valor dos juros na {monthly_meta} parcela é de R${meta_juros:.2f}."
     
     return (question, correct_answer)
 
 # Define a função que verifica a resposta do usuário
 def check_answer(question, correct_answer, user_answer):
-    if user_answer == correct_answer:
+    if (user_answer - correct_answer) <= 0.01:
         return "Resposta correta!"
     else:
         return "Resposta incorreta. A resposta correta é: " + correct_answer
